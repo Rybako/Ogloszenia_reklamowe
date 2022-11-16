53cbe48e57b7172f16dba59b05c4c3d742f57412
mlukaszczyk <mlukaszczyk@mailo.com>
2022-11-16 19:25:20 +0100

    Listing controller quickfix

--- app/Http/Controllers/ListingItemController.php
+++ app/Http/Controllers/ListingItemController.php
@@ -6,24 +6,24 @@ use DateTime;
 use Exception;
 use DatePeriod;
 use DateInterval;
-use App\models\ListingItem;
+use App\Models\ListingItem;
 use Illuminate\Http\Request;
-use App\models\ListingPictures;
+use App\Models\ListingPictures;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class ListingItemController extends Controller
 {
     //
-    
+
     function index(){ // Domyślny widok ogłoszeń wyswietla określoną liczbę ostatnio dodanych
         $listing_items = ListingItem::orderBy('add_date', 'desc')->paginate(8);
         foreach($listing_items as $key=>$item){
             $listing_items[$key]['src']=(ListingPictures::where('listing_item_id','=', $item['id'])->orderBy('order_position', 'asc')->first())['src'];
             }
-        
+
         return view('listing_item/search',['listing_items' => $listing_items]);
-        
+
     }
     function search(Request $search_data){
         $price_minimum = $search_data->has('price_min') ? $search_data->get('price_min') : 0;
@@ -31,7 +31,7 @@ class ListingItemController extends Controller
         $height_minimum = $search_data->has('height_min') ? $search_data->get('height_min') : 0;
         $width_minimum = $search_data->has('width_min') ? $search_data->get('width_min') : 0;
         $sort = $search_data->has('sort') ? $search_data->get('sort') : 'new';
-        
+
         $listing_items = ListingItem::where( [['price', '>=', (int)$price_minimum], ['price', '<=', (int)$price_maximum],
         ['height', '>=', (int)$height_minimum], ['width', '>=', (int)$width_minimum]]
                                             );
@@ -44,14 +44,14 @@ class ListingItemController extends Controller
         if($sort == 'new') $listing_items= $listing_items ->orderBy('add_date', 'desc')->paginate(8);
         if($sort == 'cheap') $listing_items= $listing_items ->orderBy('price', 'asc')->paginate(8);
         if($sort == 'expensive') $listing_items= $listing_items ->orderBy('price', 'desc')->paginate(8);
-                
 
-        
-        
+
+
+
         return view('listing_item/search',['listing_items' => $listing_items]);
     }
     function create(){
-        
+
         return view('listing_item/create');
     }
     function create_form(Request $create_data){
@@ -76,7 +76,7 @@ class ListingItemController extends Controller
         $images=[];
         $date = new DateTime();
         $add_date =  $date->format('Y-m-d H:i:s');
-        $date->add(new DateInterval('P10D')); 
+        $date->add(new DateInterval('P10D'));
         $expiration_date = $date->format('Y-m-d H:i:s');
         error_log(json_encode($images));
         foreach($create_data->images as $key => $image){
@@ -88,7 +88,7 @@ class ListingItemController extends Controller
             DB::beginTransaction();
         $id = ListingItem::insertGetId(
             [
-            'title' => $title, 
+            'title' => $title,
             'price' => $price,
             'height' => $height,
             'width' => $width,
@@ -99,14 +99,14 @@ class ListingItemController extends Controller
             'position_X' => '1234',// position x for map addon
             'position_Y' => '543321' // position y for map addon
             ]
-        );  
+        );
         foreach($images as $key => $imageName){
         ListingPictures::insertGetId(
             [
             'listing_item_id' => $id,
             'order_position' => $key,
             'src' => $imageName
-            ]    
+            ]
         );
         }
         DB::commit();
@@ -114,8 +114,8 @@ class ListingItemController extends Controller
             DB::rollback();
 
         }
-        
-        if( empty($id) ){    
+
+        if( empty($id) ){
             redirect()->back()->with('error', 'Coś nie działa!');
          }
         else {
@@ -127,6 +127,6 @@ class ListingItemController extends Controller
         $images = ListingPictures::where('listing_item_id','=',$id)->orderBy('order_position', 'asc')->get();
         return view('listing_item/view',['item' => $item,'images' => $images]);
     }
-    
+
 
 }
