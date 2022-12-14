976a082b4358fe8fedb2fd45bbf4ecb704b2b42c
JestemMarcin <45733550+JestemMarcin@users.noreply.github.com>
2022-12-14 15:39:32 +0100

    backlog edit again

--- routes/web.php
+++ routes/web.php
@@ -33,14 +33,13 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 //testowa implementacja mailowania
 Route::get('/send',[MailController::class,'index']);
 
-//użykownicy by o120d6
-Route::get('/użytkownik/{id}', [UserController::class,'view'])->name('user.view'); // nie wiem czy dobrze tu jest kontroler
+//Użykownicy
+Route::get('/użytkownik/{id}', [UserController::class,'view'])->name('user.view');
 
 //Panel użytkownika
-
 Route::get('/userpanel',  function () {
     return redirect(route('userpanel.listing_items'));
-})->name('userpanel.view')->middleware('auth'); // tu trzeba będzie zmienić kontroler potem, albo dodać nowy
+})->name('userpanel.view')->middleware('auth'); 
 Route::get('/userpanel/listing_items', [UserPanelController::class,'listing_items'])->name('userpanel.listing_items')->middleware('auth');
 
 //Obrazki
@@ -52,9 +51,10 @@ Route::get('/ogloszenia', [ListingItemController::class,'index'])->name('listing
 Route::any('/ogloszenia/szukaj', [ListingItemController::class,'search'])->name('listing_item.search');
 Route::get('/ogloszenia/dodaj', [ListingItemController::class,'create'])->name('listing_item.create')->middleware('auth');
 Route::get('/ogloszenia/edytuj/{id}', [ListingItemController::class,'edit'])->name('listing_item.edit')->middleware('auth');
-Route::any('/ogloszenia/edytuj_formularz/{id}', [ListingItemController::class,'edit_form'])->name('listing_item.edit_form')->middleware('auth');
-Route::any('/ogloszenia/dodaj_formularz', [ListingItemController::class,'create_form'])->name('listing_item.create_form')->middleware('auth');
 Route::get('/ogloszenia/widok/{id}', [ListingItemController::class,'view'])->where(['id' => '[0-9]{1,5}'])->name('listing_item.view');
 
+//Ogloszenia manipulacja
+Route::any('/ogloszenia/edytuj_formularz/{id}', [ListingItemController::class,'edit_form'])->name('listing_item.edit_form')->middleware('auth');
+Route::any('/ogloszenia/dodaj_formularz', [ListingItemController::class,'create_form'])->name('listing_item.create_form')->middleware('auth');
 Route::get('/ogloszenia/delete/{id}', [ListingItemController::class,'delete'])->name('listing_item.delete')->middleware('auth');
 Route::get('/ogloszenia/add_time/{id}', [ListingItemController::class,'add_time'])->name('listing_item.add_time')->middleware('auth');
